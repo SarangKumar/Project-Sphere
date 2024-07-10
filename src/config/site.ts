0befb67469f117e83797de9c30481385428fd53f
SarangKumar <sarangkumar1578@gmail.com>
2024-07-11 00:56:29 +0530

    chore: Update site description in site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   url: "http://project-sphere.vercel.app",
   ogImage: "http://project-sphere.vercel.app/og.jpg",
   description:
-    "Project Sphere is a collaborative platform that allows users to create and manage projects. Users can sign in, create projects, and share them with others through a link. The platform also integrates Stripe for subscriptions, allowing users to create more projects. Each project includes a built-in ChatGPT assistant to help with various tasks.",
+    "Project Sphere is a collaborative platform that allows users to create and manage projects. Users can sign in, create projects, share, manage it and more",
   links: {
     github: "https://github.com/sarangKumar/project-sphere",
   },
