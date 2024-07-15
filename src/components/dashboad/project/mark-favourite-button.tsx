810842f78cb867af76f0343022e3df255c6f61dd
SarangKumar <sarangkumar1578@gmail.com>
2024-07-15 10:09:54 +0530

    chore: pushing pseudo favourite button

---
+++ src/components/dashboad/project/mark-favourite-button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MarkFavouriteButton = ({
+  userId,
+  isFavourite,
+}: {
+  userId: string;
+  isFavourite: boolean;
+}) => {
+  return <div>MarkFavouriteButton</div>;
+};
+
+export default MarkFavouriteButton;
